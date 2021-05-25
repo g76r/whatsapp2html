a71e014074de851a0aed69e9b264dcb54ab66145
Grégoire Barbier <devel@g76r.eu>
2021-05-25 17:44:27 +0200

    supporting multiline messages

--- whatsapp2html.sh
+++ whatsapp2html.sh
@@ -4,21 +4,29 @@ gawk '
 BEGIN {
   prev_d=""
   right_s="'"$1"'"
+  print "<p>"
 }
 match($0, /^\[([^,]*),([^\]]*)\] ([^:]*): (.*)$/, a) {
   h=a[1]
   d=a[2]; s=a[3]; m=a[4];
   if (d != prev_d) {
-    print "<p class=day>" d "</p>"
+    print "</p><p class=day>" d
     prev_d = d
   }
   if (right_s == "") {
     right_s = s
   }
   if (s == right_s) {
-    print "<p class=rs>" h " " s " : " m "</p>"
+    print "</p><p class=rs>" h " " s " : " m
   } else {
-    print "<p class=ls>" h " " s " : " m "</p>"
+    print "</p><p class=ls>" h " " s " : " m
   }
+  next
+}
+{
+  print "<br/>" $0
+}
+END {
+  print "</p>"
 }
 '
